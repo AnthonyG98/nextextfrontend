3cab73b3d0b713f65500c34abc0750a196b81590
Anthony Gonzalez <gonz.anthony6@gmail.com>
2023-02-03 11:28:39 -0800

    Working extension needs CSS

--- scripts/content.js
+++ scripts/content.js
@@ -1,43 +1,32 @@
-let url = "http://localhost:3001";
-let responseFromLogin;
+// Setting initial time of 0
+let timeElement = document.getElementById("time");
+let timeSet = parseInt(timeElement.innerHTML);
 
-const onLogin = async () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    let urlForTask = tabs[0].url;
-    alert(urlForTask);
-    // use `url` here inside the callback because it's asynchronous!
-  });
-  const loginData = {
-    username: document.getElementById("username").value,
-    password: document.getElementById("password").value,
-  };
-  fetch(`${url}/users/login`, {
-    method: "POST",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: loginData.username,
-      password: loginData.password,
-    }),
-  })
-    .then((response) => response.json())
-    .then(
-      (response) =>
-        chrome.storage.sync
-          .set({ key: JSON.stringify(response.id) })
-          .then(() => {
-            // alert(`Your response is ${JSON.stringify(response.id)}`);
-            alert(urlForTask);
-          })
-      // chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-      //   const activeTab = tabs[0];
-      //   chrome.tabs.sendMessage(activeTab.id, { command: "openModal" });
-      // })
-    );
+//Adding and subtracting time
+const addTime = () => {
+  timeSet += 5;
+
+  document.getElementById("time").innerHTML = timeSet;
+};
+const subtractTime = () => {
+  timeSet -= 5;
+
+  document.getElementById("time").innerHTML = timeSet;
 };
 
-const loginBtn = document
-  .getElementById("login-btn")
-  .addEventListener("click", onLogin);
+document.getElementById("add").addEventListener("click", addTime);
+
+document.getElementById("subtract").addEventListener("click", subtractTime);
+//Change URL to swtich to once time runs out
+const changeUrl = () => {
+  let tabUrl = document.getElementById("url-input").value;
+  chrome.tabs.update(undefined, { url: tabUrl });
+};
+//Converts user input minutes into milliseconds
+function minutesToMilliseconds(minutes) {
+  return setTimeout(changeUrl, minutes * 60 * 1000);
+}
+//Execute extension functionality
+document.getElementById("set-btn").addEventListener("click", () => {
+  minutesToMilliseconds(timeSet);
+});
